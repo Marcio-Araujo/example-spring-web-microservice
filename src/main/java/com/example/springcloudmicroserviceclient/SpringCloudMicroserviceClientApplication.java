28647210443fe70b923125f51a3e3f00f2aee19b
maritsu <maritsu2015@gmail.com>
2018-12-01 18:19:44 -0200

    configuration files and base project

---
+++ src/main/java/com/example/springcloudmicroserviceclient/SpringCloudMicroserviceClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudmicroserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMicroserviceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMicroserviceClientApplication.class, args);
+	}
+}
